506b451f0c4685ad6c3c95dd1727d58d8686db05
Phips Peter <pspeter333@gmail.com>
2013-02-15 14:06:29 -0800

    Installed lodash

--- public/js/piano.js
+++ public/js/piano.js
@@ -1,9 +1,6 @@
 $(document).ready(function() {
 	
-	var socket = io.connect('http://localhost');
-  	socket.on('helo', function (data) {
-    console.log(data);
-  });
+	
 
 	var $audio = $('audio#tone-3C');
   var $key = $('.piano-key');
@@ -15,5 +12,12 @@ $(document).ready(function() {
 		tone.pause();
 		tone.currentTime = 0;
 		tone.play();
+  });
+
+  var socket = io.connect('http://localhost');
+  socket.on('note', function (data) {
+    console.log(data.id);
+    var tone = $audio.get(data.id);
+    tone.play();
   });
 });
