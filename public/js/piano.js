df4d84bcc341c852263494e8c828af2003b089eb
Alex Corre <aecorre@linkedin.com>
2013-02-15 14:08:28 -0800

    added setup

--- public/js/piano.js
+++ public/js/piano.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
+	// initialize socket to listen to localhost
+	var socket = io.connect('http://localhost');
+	
+	// hello test message
+	socket.on('helo', function (data) {
+		console.log(data);
+	});
 
+	// setup callback
+	// data of format
+	// {
+	//  	groupId: 1,
+	//		numGroups: 5
+	// }
+	socket.on('setup', function(data){
+		clientState.groupId = data.groupId || clientState.groupId;
+		clientState.numGroups = data.numGroups || clientState.numGroups;
+	});
 
 	var $audio = $('audio#tone-3C');
 	var $key = $('.piano-key');
